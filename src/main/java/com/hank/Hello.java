e18778a7cbbfdf2226a15f737c2e6f3c13ae5e7c
cavabien0527 <cavabien0527@gmail.com>
2020-05-23 13:34:18 +0800

    FirstCommit_20200523

---
+++ src/main/java/com/hank/Hello.java
@@ -0,0 +1,11 @@
+package com.hank;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Nice~");
+        Person p = new Person();
+        p.hello();
+    }
+    }
+
---
+++ src/main/kotlin/com/hank/Hello.kt
@@ -0,0 +1,16 @@
+package com.hank
+
+fun main() {
+//    println("Hello Kotlin!\n This is the practice file")
+//    Human().Hello()
+    val h = Human()
+    h.Hello()
+
+}
+
+class Human{
+    fun Hello(){
+        println("Hello Kotlin!")
+    }
+
+}
